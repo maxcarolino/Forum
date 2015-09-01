ef7c94b10e36b584b7f59bbd36ccd26aa7941861
Maximino Carolino III <m.carolino@klab.com>
2015-09-01 12:25:38 +0800

    Implemented thread list and comment viewing + validation

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -5,11 +5,12 @@ require_once APP_DIR.'app_model.php';
 require_once APP_DIR.'app_layout_view.php';
 require_once APP_DIR.'app_exception.php';
 
-// vendor
-require_once VENDOR_DIR.'SimpleDBI/SimpleDBI.php';
-
 // helpers
 require_once HELPERS_DIR.'html_helper.php';
+require_once HELPERS_DIR.'validation_helper.php';
+
+// vendor
+require_once VENDOR_DIR.'SimpleDBI/SimpleDBI.php';
 
 // config
 require_once CONFIG_DIR.'log.php';
---
+++ app/models/thread.php
@@ -0,0 +1,61 @@
+<?php
+
+class Thread extends AppModel
+{
+   public static function get($id)
+   {
+      $db = DB::conn();
+
+      $row = $db->row('SELECT * FROM thread WHERE id = ?', 
+      array($id)
+      );
+
+      if(!$row) {
+	 throw new RecordNotFoundException('No record found!');
+      }
+
+      return new self($row);
+   }
+	
+   public static function getAll()
+   {
+      $threads = array();
+      $db = DB::conn();
+      $rows = $db->rows('SELECT * FROM thread');
+
+      foreach ($rows as $row) {
+	 $threads[] = new Thread($row);
+      }
+
+      return $threads;
+   }
+
+   public function getComments()
+   {
+      $comments = array();
+      $db = DB::conn();
+
+      $rows = $db->rows('SELECT * FROM comment WHERE thread_id = ? ORDER BY created ASC',
+      array($this->id)
+      );
+
+      foreach ($rows as $row) {
+         $comments[] = new Comment($row);
+      }
+
+      return $comments;
+   }
+
+   public function write(Comment $comment)
+   {
+      if(!$comment->validate()) {
+	 throw new ValidationException('Invalid comment');
+      }
+	
+      $db = DB::conn();
+
+      $db->query('INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()',
+      array($this->id, $comment->username, $comment->body)
+      );
+   }
+}
