a7c937b4e00beadcd525cac38954ac6a4d633368
Maximino Carolino III <m.carolino@klab.com>
2015-09-07 12:26:13 +0800

    simplify codes

---
+++ app/views/user/log_in_end.php
@@ -0,0 +1,8 @@
+<h1 class="text-center"> Welcome, <?php eh($_SESSION['username']) ?>!</h1></br>
+<div class="row">
+  <div class="col-md-4 col-md-offset-5">
+    <a href="http://10.3.140.112/thread/index" role="button" class="btn btn-primary">
+    <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+    View Threads</a>
+  </div>
+</div>
